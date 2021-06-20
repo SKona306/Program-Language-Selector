7e617f4671279fc1fecf3265124bd22be313753b
Shaun Kent <shaunkent81@gmail.com>
2021-06-19 18:11:51 -0700

    add initial branching to survey

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $("form#codeform").submit(function(event){
+    event.preventDefault();
+    const answerOne = parseInt($("input:radio[name=q1]:checked").val());
+    const answerTwo = parseInt($("input:radio[name=q2]:checked").val());
+    const answerThree = parseInt($("input:radio[name=q3]:checked").val());
+    const answerFour = parseInt($("input:radio[name=q4]:checked").val());
+    const answerFive = parseInt($("#q5").val());
+    const answerSix = parseInt($("#q6").val());
+    const result = answerOne + answerTwo + answerThree + answerFour + answerFive + answerSix;
+
+    if(result <= 9){
+      $(".language").text("Javascript");
+      $("#final-results").show();
+    }else if (result >= 10 && result <= 13){
+      $(".language").text("C#");
+      $("#final-results").show();
+    }
+  });
+});
